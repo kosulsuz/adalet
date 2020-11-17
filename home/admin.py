dc5cdfa00e83cb9fb2564897347efbe95879f0fc
sahinturk <kopuskopecik@gmail.com>
2020-11-17 19:40:36 +0200

    Messages added into dmin

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Setting, About, AboutLogo, Skill, Testimonials, Resume, Service, CompanyType, Reference
+from .models import Setting, About, AboutLogo, Skill, Testimonials, Resume, Service, CompanyType, Reference, ContactInfo
 
 class SettingAdmin(admin.ModelAdmin):
 		
 	list_display = ['title', 'icon', 'youtube_url','header', 'content', 'description']
+
+class ContactInfoAdmin(admin.ModelAdmin):
+		
+	list_display = ['name', 'email', 'topic', "content"]
 
 class ResumeAdmin(admin.ModelAdmin):
 		
@@ -41,6 +45,7 @@ admin.site.register(Setting, SettingAdmin)
 admin.site.register(About, AboutAdmin)
 admin.site.register(Resume, ResumeAdmin)
 admin.site.register(CompanyType, CompanyTypeAdmin)
+admin.site.register(ContactInfo, ContactInfoAdmin)
 admin.site.register(Service)
 
 
